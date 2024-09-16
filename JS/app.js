8f86cc48a2351e752ee4346daeab166fddf7519b
Shamoil <139974413+shamoil-khan@users.noreply.github.com>
2024-09-16 18:24:28 +0500

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,122 @@
+const icons = document.querySelector(".icons");
+const navbar = document.querySelector("header nav");
+const navLinks = document.querySelectorAll("header nav a");
+// const completeSourceBtn = document.querySelector(".complete");
+
+icons.addEventListener("click", () => {
+  icons.classList.toggle("close");
+  navbar.classList.toggle("active");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    if (navbar.classList.contains("active")) {
+      icons.classList.remove("close");
+      navbar.classList.remove("active");
+    }
+  });
+});
+
+// completeSourceBtn.addEventListener('click', () => {
+//   window.open('comingSoon.html')
+// })
+
+//?  FOR CONTACT
+
+const form = document.querySelector("form");
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const subject = document.getElementById("subject");
+const mess = document.getElementById("message");
+
+function sendEmail() {
+  const bodyMessage = `Full Name: ${fullName.value}<br /> Email: ${email.value}<br />
+  Phone Number: ${phone.value}<br /> Message: ${mess.value}`;
+
+  Email.send({
+    SecureToken: "65f8fc6d-90ea-4c86-be62-b1718af05792",
+    To: "shamoilkhan210@gmail.com",
+    From: "shamoilkhan210@gmail.com",
+    Subject: subject.value,
+    Body: bodyMessage,
+  }).then((message) => {
+    if (message == "OK") {
+      Swal.fire({
+        title: "Success!",
+        text: "Message sent successfully!",
+        icon: "success",
+      });
+    }
+  });
+}
+
+function checkInputs() {
+  const items = document.querySelectorAll(".item");
+
+  for (const item of items) {
+    if (item.value == "") {
+      item.classList.add("error");
+      item.parentElement.classList.add("error");
+    }
+
+    if (items[1].value != "") {
+      checkEmail();
+    }
+
+    items[1].addEventListener("keyup", () => {
+      checkEmail();
+    });
+
+    item.addEventListener("keyup", () => {
+      if (item.value != "") {
+        item.classList.remove("error");
+        item.parentElement.classList.remove("error");
+      } else {
+        item.classList.add("error");
+        item.parentElement.classList.add("error");
+      }
+    });
+  }
+}
+
+function checkEmail() {
+  const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
+  const errorTxtEmail = document.querySelector(".error-txt.email");
+
+  if (!email.value.match(emailRegex)) {
+    email.classList.add("error");
+    email.parentElement.classList.add("error");
+
+    if (email.value != "") {
+      errorTxtEmail.innerText = "Enter a valid email address";
+    } else {
+      errorTxtEmail.innerText = "Email Address can't be blank";
+    }
+  } else {
+    email.classList.remove("error");
+    email.parentElement.classList.remove("error");
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkInputs();
+
+  if (
+    !fullName.classList.contains("error") &&
+    !email.classList.contains("error") &&
+    !phone.classList.contains("error") &&
+    !subject.classList.contains("error") &&
+    !mess.classList.contains("error")
+  ) {
+    try {
+      sendEmail();
+    } catch (error) {
+      console.error(error);
+    }
+
+    form.reset();
+    return false;
+  }
+});
---
+++ JS/download.js
@@ -0,0 +1,22 @@
+const downloadBtn = document.querySelectorAll(".downloadBtn");
+const watchBtn = document.querySelectorAll(".watchBtn");
+
+downloadBtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    let aTag = document.createElement("a");
+    aTag.setAttribute("href", e.target.getAttribute("data-src"));
+    aTag.setAttribute("download", "ex");
+    document.body.appendChild(aTag);
+
+    aTag.click();
+
+    aTag.remove();
+  });
+});
+
+for (var i = 0; i < watchBtn.length; i++) {
+  watchBtn[i].addEventListener("click", (e) => {
+    var target = e.target;
+    window.open(target.getAttribute("data-src"));
+  });
+}
